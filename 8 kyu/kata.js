b7f5d58e062147c5ef01aeca617742ff1ae45a61
ToniNov <antoni.novik@yandex.ru>
2022-03-17 00:32:53 +0300

    1.Beginner Series #4 Cockroach

--- 8 kyu/kata.js
+++ 8 kyu/kata.js
@@ -5,3 +5,7 @@ const greet = (name, owner) => (name == owner) ? 'Hello boss' :'Hello guest';
 //Opposite number
 
 const opposite = (number) => Math.sign(number) < 0 ? Math.abs(number) : -Math.abs(number);
+
+//Beginner Series #4 Cockroach
+
+const cockroachSpeed = s => Math.floor(s * (100000 / 3600))
