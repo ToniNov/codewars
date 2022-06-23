b0839f7382d51ffc416e6133077942a91ae25c8a
ToniNov <antoni.novik@yandex.ru>
2022-06-23 23:26:08 +0300

    6.Transpose two strings in an array

--- 7 kyu/kata.js
+++ 7 kyu/kata.js
@@ -13,3 +13,18 @@ function isIsogram(str) {
         }
     });
 }
+
+//7 kyu
+//Transpose two strings in an array
+
+function transposeTwoStrings(arr){
+    let result = [];
+    let len = Math.max(arr[0].length, arr[1].length);
+
+    for (let i = 0; i < len; i++) {
+        var column = (arr[0][i] || " ") + " " + (arr[1][i] || " ");
+        result.push(column);
+    }
+
+    return result.join("\n");
+}
